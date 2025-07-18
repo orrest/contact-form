ffb30477d47401fc0749a2eccd003e46ea78791c
orrest <zzhxufeng@163.com>
2025-07-18 14:55:26 +0800

    contact-page: error msg

--- src/app/contact-page/contact-page.ts
+++ src/app/contact-page/contact-page.ts
@@ -5,11 +5,10 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-page.html',
   styleUrl: './contact-page.css',
 })
@@ -17,6 +16,8 @@ export class ContactPage {
   private fb = inject(FormBuilder);
 
   form: FormGroup;
+
+  requiredErr = 'This field is required';
 
   constructor() {
     this.form = this.fb.group({
@@ -29,9 +30,25 @@ export class ContactPage {
     });
   }
 
+  getField(name: string) {
+    return this.form.get(name);
+  }
+
+  isFieldInvalid(name: string) {
+    const field = this.getField(name);
+    if (field) {
+      return field?.invalid && (field?.dirty || field?.touched);
+    }
+
+    return false;
+  }
+
   onSubmit(form: FormGroup) {
     if (form.valid) {
       // submit success
+    } else {
+      // 将所有表单控件标记为 touched，以显示所有错误
+      form.markAllAsTouched();
     }
   }
 }
